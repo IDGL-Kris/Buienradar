a9470dd9179c618975bbb549155dd425121204bd
IDGL-Kris <kristianvberlo@gmail.com>
2022-10-15 11:36:38 +0200

    cleaning

--- Program.cs
+++ Program.cs
@@ -16,14 +16,10 @@ while (i < count)
     {
         Console.WriteLine($"{prop.Name} : {prop.GetValue(results, null).ToString()}");
     }
-
-
-    // var path = Path.GetFullPath("/home/kris/repo/testing/pullDatatest/pullingdata");
-
     using StreamWriter file = new("weather.txt", append: true);
-
     var prefix = $"Current DateTime: {DateTime.Now} with index {i}, ";
     await file.WriteLineAsync(prefix + stringresponse);
+    
     Thread.Sleep(600000);
     i++;
 }
